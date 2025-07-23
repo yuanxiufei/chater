5196b8548c3509bf992eab468b7d169e1d06b5d8
ReginYuan <2496943860@qq.com>
2025-07-23 10:25:23 +0800

    feat: 同步代码

---
+++ src/Refueling.java
@@ -0,0 +1,7 @@
+public class Refueling {
+    public static void main(String[] args) {
+        System.out.println("山高水苑，相伴永远");
+        System.out.println("总价格是："+ 9*50+"元");
+        System.out.println("总价格是："+(5000 + 9*50*0.95)+"元");
+    }
+}
